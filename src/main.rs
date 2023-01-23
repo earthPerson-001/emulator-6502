295bdaad845104ee2073954335c8dea7065ed298
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-01-23 21:46:55 +0545

    Main: Removed panic at runtime and displaying the instruction set on debug build.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-pub mod processor;
-pub mod memory;
 pub mod bus;
+pub mod memory;
+pub mod processor;
 
+use crate::processor::Instruction;
 
-fn main() {
-    panic!("Implementation Incomplete!");
+fn display_instruction_set() -> () {
+    let vec = Instruction::create_instructions_table();
+    println!("length of vector: {}", vec.len());
+
+    for (i, instruction) in vec.iter().enumerate() {
+        print!("{}", instruction);
+
+        if (i + 1) % 16 == 0 {
+            println!("");
+        }
+    }
+
+}
+
+fn main() -> () {
+    #[cfg(debug_assertions)] 
+    {
+        display_instruction_set();
+    }
+
+    println!("Hello World!");
 }
