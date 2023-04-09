b8e7001cda2ee8c8d627c13247dedda5d1d8a34f
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-04-09 09:25:26 +0545

    rust wasm: Wrapping processor instance inside of RefCell to make it globally accessible, instead of writing unsafe block everywhere. Following camelCase notation for function exposed to JS. Added clear_processor_instance function.

--- src/wasm_interface.rs
+++ src/wasm_interface.rs
@@ -9,6 +9,9 @@ pub use processor::Processor;
 
 use wasm_bindgen::prelude::*;
 
+use serde_json;
+use std::cell::RefCell;
+
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
@@ -27,44 +30,120 @@ struct Instance {
     total_clock_cycle: u16,
 }
 
-// An instance for a page
-static mut INSTANCE: Instance = Instance {
-    processor: None,
-    total_clock_cycle: 0,
-};
+/*
+ * As JS is single threaded, this won't be a problem
+ */
+thread_local! (
+    static INSTANCE: RefCell<Instance> = RefCell::new( Instance{processor: None, total_clock_cycle: 0,} )
+);
 
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = createProcessor)]
 // creating processor
-// unsafe method for now
-pub unsafe fn create_processor() {
-    INSTANCE.processor = Some(Processor::new());
-    INSTANCE.total_clock_cycle = 0;
+pub fn create_processor() {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+
+        instance.processor = Some(Processor::new());
+        instance.total_clock_cycle = 0;
+    });
 
     log("An instance of processor created");
 }
 
-#[wasm_bindgen]
-// the clock cycle
-// unsafe method for now
-pub unsafe fn tick_clock() {
-    match &mut INSTANCE.processor {
-        Some(proc) => {
-            proc.clock();
-            INSTANCE.total_clock_cycle += 1;
-            log("Called tick_clock through wasm");
-        },
-        None => (),
-    }
+#[wasm_bindgen(js_name=clearProcessorInstance)]
+// clearing processor
+pub fn clear_processor_instance() {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+
+        instance.processor = None;
+        instance.total_clock_cycle = 0;
+    });
+
+    log("An instance of processor created");
 }
 
-#[wasm_bindgen]
+#[wasm_bindgen(js_name=tickClock)]
+// the clock cycle
+pub fn tick_clock() {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+        match &mut instance.processor {
+            Some(proc) => {
+                proc.reset();
+                instance.total_clock_cycle += 1;
+                log("Called tick_clock through wasm");
+            }
+            None => (),
+        }
+    })
+}
+
+#[wasm_bindgen(js_name=loadRomFromFilepath)]
 // load the rom file
-// unsafe method for now
-pub unsafe fn load_rom(filepath: &str) {
-    match &mut INSTANCE.processor {
+pub fn load_rom_from_filepath(filepath: &str) {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+        match &mut instance.processor {
+            Some(proc) => {
+                proc.load_rom(filepath);
+            }
+            None => (),
+        }
+    })
+}
+
+#[wasm_bindgen(js_name=getRam)]
+pub fn get_ram() -> std::string::String {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+        match &mut instance.processor {
+        Some(proc) => serde_json::to_string(&proc.bus.memory).unwrap(),
+        None => "{}".to_owned(),
+    }})
+}
+
+#[wasm_bindgen(js_name=getRom)]
+pub fn get_rom() -> std::string::String {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+        match &mut instance.processor {
+        Some(proc) => serde_json::to_string(&proc.bus.secondary_storage).unwrap(),
+        None => "{}".to_owned(),
+    }})
+}
+
+#[wasm_bindgen(js_name=getProcessorStatus)]
+pub fn get_processor_status() -> std::string::String {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+        match &mut instance.processor {
+        Some(proc) => serde_json::to_string(&proc.status).unwrap(),
+        None => "{}".to_owned(),
+    }})
+}
+
+#[wasm_bindgen(js_name=loadRom)]
+pub fn load_rom(bytes: String) {
+    INSTANCE.with(|ins| {
+        let mut instance = ins.borrow_mut();
+        match &mut instance.processor {
         Some(proc) => {
-            proc.load_rom(filepath);
-        },
-        None => (),
-    }
+            // converting string to hexadecimal
+            let nums: Vec<u8> = bytes
+                .chars()
+                .map(|c| c.to_digit(16).expect("Conversion Error") as u8)
+                .collect();
+
+            log(format!("Loaded {:?}", nums).as_str());
+
+            // changing the rom
+            for (i, val) in nums.iter().enumerate() {
+                if i < proc.bus.secondary_storage.len() {
+                    proc.bus.secondary_storage[i as u16] = *val;
+                }
+            }
+        }
+        None => {}
+    }})
 }
