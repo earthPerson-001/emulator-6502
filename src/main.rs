8852f18dc43669feb3bdbe5b39e210a141355fc5
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-04-15 11:45:33 +0545

    rust wasm: Add disassembly output for easier debugging.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ pub mod memory;
 pub mod processor;
 pub mod rom;
 
-use std::env;
+use std::{env, path::Path, time::Duration};
 
 #[cfg(debug_assertions)]
 fn display_instruction_set() -> () {
@@ -21,54 +21,83 @@ fn display_instruction_set() -> () {
 
     for (i, instruction) in vec.iter().enumerate() {
         if (i == 0) || i % 16 == 0 {
-            print!("{:^12}", format!("\n {:01x}", (i + 1)/16));
+            print!("{:^12}", format!("\n {:01x}", (i + 1) / 16));
         }
 
         print!("{}", instruction);
     }
-
 }
 
 fn main() -> () {
-    #[cfg(debug_assertions)] 
+    #[cfg(debug_assertions)]
     {
         display_instruction_set();
     }
 
+    const PROGRAM_COUNTER: u16 = 0x8000;
+
     // new processor instance
-    let mut proc = Processor::new();
-    
-    let mut current_dir = String::from("");
-    match env::current_dir() {
-        Ok(current_dir_temp) => { 
-            current_dir = current_dir_temp.to_str().unwrap().to_owned();
-        },
-        Err(_) => {
-            current_dir = String::from("None");
-        },
-    }
+    let mut proc = Processor::new_setup(Some(PROGRAM_COUNTER));
 
-    let file_path = current_dir +  "/6502_functional_test.bin";
-    // loading the rom file
-    if proc.load_rom(file_path.as_str()) {
-        println!("Read File {} success", file_path);
+    let current_dir = match env::current_dir() {
+        Ok(temp) => Some(Path::new(&temp).to_owned()),
+        Err(_) => None,
+    };
+    let rom_name = r"rom_1";
 
-        let max_cycles = 10000000;
-
-        // running the cpu
-        let mut cycle_count = 0;
-        loop {
-            proc.clock();
-
-            if cycle_count > max_cycles {
-                break
-            }
-            cycle_count += 1;
+    let full_rom_path = match current_dir {
+        Some(dir) => {
+            Some(dir.join("roms").join(format!("{rom_name}.bin")))
         }
-        println!("Program Complete");
+        None => None
+    };
 
-    } else {
-        println!("Read File {} failed", file_path);
+    if let Some(rom_path) = &full_rom_path {
+        // loading the rom file
+        if let Some(rom_path) = rom_path.to_str() {
+            #[cfg(debug_assertions)]
+            {
+                println!("\nTrying to read rom from path {:?}", rom_path)
+            }
+
+            if proc.load_rom(rom_path, &PROGRAM_COUNTER) {
+                println!("Read File {} success", rom_path);
+
+                let max_cycles = 10000000;
+
+                // running the cpu
+                let mut cycle_count = 0;
+                loop {
+                    let anything: Vec<String> = proc.disassembly(&proc.program_counter, &10)
+                        .iter()
+                        .map(|(_, human_readable)| format!("{}", human_readable))
+                        .collect();
+
+                    proc.clock();
+
+                    if cycle_count > max_cycles {
+                        break;
+                    }
+                    cycle_count += 1;
+
+                    (1..20).into_iter().map(|_| print!("-")).for_each(drop);
+                    println!("");
+                    for thing in anything {
+                        println!("{}", thing);
+                    }
+                    (1..20).into_iter().map(|_| print!("-")).for_each(drop);
+
+                    // wait for 1 sec
+                    std::thread::sleep(Duration::new(1, 0))
+                }
+                println!("Program Complete");
+                return;
+            }
+        }
     }
 
+    #[cfg(debug_assertions)]
+    {
+        println!("\nRead ROM File {:?} failed", &full_rom_path);
+    }
 }
